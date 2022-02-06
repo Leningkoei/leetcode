30c3dda65dee405d01c38e57726b95a4d8372984
Leningkoei <zhanglening2001@gmail.com>
2022-02-06 17:23:32 +0900

    2022/02/06 Rename Problems.

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,19 @@
-console.log('nmsl')
+import fs from "fs";
+import path from "path";
+
+const main = async () => {
+    fs.readdir(path.resolve("src", "problems"), (err, files) => {
+        files.forEach(file => {
+            const newName = file.split("_").join("-");
+
+            fs.rename(
+                path.resolve("src", "problems", file),
+                path.resolve("src", "problems", newName),
+                err => { throw err }
+            );
+        });
+    });
+};
+
+main();
 
