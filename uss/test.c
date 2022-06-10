7b13961611653aa03d93d8c297efd363695d91a6
Leningkoei <zhanglening2001@gmail.com>
2022-06-10 03:16:09 +0000

    Oops

--- uss/test.c
+++ uss/test.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+  return 0;
+};
+
 int main() {
   int a = 1;
-  int *b = &a;
-  int c = ++*b++*++*b++;
-  // int c = ++*b++*++*b++*++*b++;
-  // int d = ++c*++*b++*c++*++*b++*c++;
-
-  printf("%d\n", c);
-  // printf("%d\n", d);
-
+  int b = 2;
+  swap(&a, &b);
+  printf("%d, %d\n", a, b);
+  
   return 0;
 };
